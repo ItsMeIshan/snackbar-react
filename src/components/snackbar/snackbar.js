a35fd01d350cc7cf217a1b94e50487d380eec6a6
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2023-07-14 10:15:44 +0530

    adds basic snackbar component

---
+++ src/components/snackbar/snackbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  AlertOctagon,
+  AlertTriangle,
+  CheckCircle,
+  Info,
+  X,
+} from "react-feather";
+
+const ICONS = {
+  notice: Info,
+  warning: AlertTriangle,
+  success: CheckCircle,
+  error: AlertOctagon,
+};
+
+function Toast() {
+  return (
+    <div>
+      <div>
+        <Info size={24} />
+      </div>
+      <p>16 photos have been uploaded</p>
+      <button>
+        <X size={24} />
+      </button>
+    </div>
+  );
+}
+
+export default Toast;
